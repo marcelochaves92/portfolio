69cba4f16a73625fe969c8a068bf55405bb87842
Marcelo Chaves <9marceli@gmail.com>
2023-02-27 17:44:58 -0300

    Changes

--- components/About.js
+++ components/About.js
@@ -2,23 +2,23 @@ import { FaChevronDown } from "react-icons/fa";
 import ScrollIntoView from "react-scroll-into-view";
 
 function About() {
-  return (
-    <div className="max-w-4xl mt-20 mx-auto">
-      <p className="text-2xl text-black md:text-4xl font-bold text-center">
-        Hey 👋
-      </p>
-      <p className="text-base sm:text-xl text-center text-gray-600 leading-relaxed mt-4">
-        I'm a passionate web developer. I love creating projects and solutions
-        for people in my life.
-      </p>
+    return (
+        <div className="max-w-4xl mt-20 mx-auto">
+            <p className="text-2xl text-black md:text-4xl font-bold text-center">
+                Hey 👋
+            </p>
+            <p className="text-base sm:text-xl text-center text-gray-600 leading-relaxed mt-4">
+                I'm a passionate web developer. I love creating projects and solutions
+                for people in my life.
+            </p>
 
-      <ScrollIntoView selector="#tech">
-        <div className="mx-auto p-20">
-          <FaChevronDown className="animate-bounce mx-auto text-3xl text-blue-500" />
+            <ScrollIntoView selector="#tech">
+                <div className="mx-auto p-20">
+                    <FaChevronDown className="animate-bounce mx-auto text-3xl text-blue-500" />
+                </div>
+            </ScrollIntoView>
         </div>
-      </ScrollIntoView>
-    </div>
-  );
+    );
 }
 
 export default About;
--- pages/index.js
+++ pages/index.js
@@ -9,27 +9,27 @@ import Aos from "aos";
 import "aos/dist/aos.css";
 
 function App() {
-  useEffect(() => {
-    Aos.init({
-      once: true,
+    useEffect(() => {
+        Aos.init({
+            once: true,
+        });
     });
-  });
 
-  return (
-    <div className="min-h-screen py-10 px-3 sm:px-5 bg-gray-100">
-      <div data-aos="fade-down" data-aos-duration="800">
-        <Card />
-      </div>
+    return (
+        <div className="min-h-screen py-10 px-3 sm:px-5 bg-gray-100">
+            <div data-aos="fade-down" data-aos-duration="800">
+                <Card />
+            </div>
 
-      <div data-aos="fade-up" data-aos-duration="800" data-aos-delay="400">
-        <About />
-      </div>
+            <div data-aos="fade-up" data-aos-duration="800" data-aos-delay="400">
+                <About />
+            </div>
 
-      <Skills />
-      <ContactMe />
-      <Footer />
-    </div>
-  );
+            <Skills />
+            <ContactMe />
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
